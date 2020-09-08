003044923ebb908bd450234282a141ebdeaa79a3
midga <midga@DESKTOP-6OJP8SK.local>
2020-09-08 12:59:47 +0200

    Added JPA model object and relationship.

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,68 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity // This class is a JPA Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // The DB will provide it
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books;
+
+	// for JPA
+	public Author() {
+	}
+
+	public Author(String firstName, String lastName, Set<Book> books) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.books = books;
+	}
+
+	/** Getters and Setters */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,72 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // The DB will provide it
+	private Long id;
+
+	private String title;
+	private String isbn;
+	
+	@ManyToMany
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+	inverseJoinColumns = @JoinColumn(name = "author_id"))
+	private Set<Author> authors;
+
+	// For JPA
+	public Book() {
+	}
+
+	public Book(String title, String isbn, Set<Author> authors) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.authors = authors;
+	}
+
+	/** Getters and Setters */
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+}
