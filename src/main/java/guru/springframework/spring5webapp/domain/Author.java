d9ac4453c5e2e3ca769a5769810b90922cc52544
midga <midga@DESKTOP-6OJP8SK.local>
2020-09-08 16:43:11 +0200

    Refactoring: moved packages

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -19,17 +20,16 @@ public class Author {
 	private String lastName;
 
 	@ManyToMany(mappedBy = "authors")
-	private Set<Book> books;
+	private Set<Book> books = new HashSet<>();
 
 	// for JPA
 	public Author() {
 	}
 
-	public Author(String firstName, String lastName, Set<Book> books) {
+	public Author(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.books = books;
 	}
 
 	/** Getters and Setters */
--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -1,4 +1,4 @@
-package repositories;
+package guru.springframework.spring5webapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
